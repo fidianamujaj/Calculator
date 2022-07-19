44af8f76251b49a5098ec25b1fc25cb890b70185
Fidiana Mujaj <fidianamujaj@hotmail.com>
2022-07-19 17:47:54 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Fidiana Mujaj on 7/19/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
